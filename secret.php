2b0ed73cf94e03906d304cd89b29db7cb7f79b48
seyoulax <114023290+seyoulax@users.noreply.github.com>
2023-03-18 23:12:07 +0300

    added secret

---
+++ secret.php
@@ -0,0 +1,3 @@
+<?php
+    $env = ['bot_token' => "your bot token", "database" => ["server" => "db server", "user" => "db user", "password" => "db user pass", "database" => "db name"], "admin_id" => /*admin`s id to send approving messages*/, "chats" => [/*group_chats id*/]];
+?>
